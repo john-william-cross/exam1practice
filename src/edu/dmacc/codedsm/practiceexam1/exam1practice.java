eafaf64500ae1a1c3a33ece2887f4384f63874cd
John Cross <jwcross1@dmacc.edu>
2019-02-17 11:15:39 -0600

    second commit for some reason...

---
+++ src/edu/dmacc/codedsm/practiceexam1/exam1practice.java
@@ -0,0 +1,22 @@
+package edu.dmacc.codedsm.practiceexam1;
+
+public class exam1practice {
+
+    final int RANDOM_INT = 2;
+    int otherRandomInt = 4;
+
+    public static void main(String[] args) {
+
+        int lastInt = 32;
+
+        exam1practice product = new exam1practice();
+
+        int lalala = product.RANDOM_INT * product.otherRandomInt * lastInt;
+
+        System.out.println("The product of these three ints is : " + lalala + ".");
+
+    }
+
+}
+
+
